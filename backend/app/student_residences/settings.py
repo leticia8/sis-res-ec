cce3e4c273b13fc1eff9db514a6729871d6d01ec
Leticia <leticialado@gmail.com>
2023-11-12 19:59:36 +0000

    Remove unnecessary var

--- backend/app/student_residences/settings.py
+++ backend/app/student_residences/settings.py
@@ -130,7 +130,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCES_KEY")
 
 if ENVIRONMENT != "local":
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    AWS_STORAGE_BUCKET_NAME = "sistemaresidenza-bucket"
+    AWS_STORAGE_BUCKET_NAME = "sistema-residenza-s3"
     AWS_S3_REGION_NAME = "sa-east-1"
     URL_FRONT = "https://sistema-residenza.vercel.app/"
     SITE_ID = 6
